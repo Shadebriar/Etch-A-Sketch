38fd9ad3c273da8bbc519b0fb0b4821641d6c6a4
Ryan <43834162+KingOzma@users.noreply.github.com>
2026-07-10 21:34:43 -0400

    Created a button and a prompt to change the grid size, while also scaling the grid properly on the screen.

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ div.addEventListener("mouseover", () => {
 })
 }
 
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    let userInput = prompt("Enter a grid size");
+    if (userInput <= 100){
+        container.textContent = "";
+    
+for(let i = 0; i < userInput * userInput; i++){
+const div = document.createElement("div");
+div.style.width = `${100 / userInput}%`
+div.style.height = `${100 / userInput}%`
+div.classList.add("square");
+
+container.appendChild(div);
+div.addEventListener("mouseover", () => {
+    div.style.background = "black";})
+}}
+})
+
